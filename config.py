5b4ba161f5ce0cbe1c2a35dd1a67a415c33ca806
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2024-12-26 20:47:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "1980"))
+    PORT = int(os.environ.get("PORT", "8008"))
 
     
     caption = """
